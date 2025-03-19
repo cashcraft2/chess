3ac31541af2a4330d89e89c34b89815274106982
cashcraft2 <cashcraft2@gmail.com>
2025-03-19 00:07:12 -0600

    Implements positive unit test for clearDatabase in the facade

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,10 +1,14 @@
 package client;
 
+import dataaccess.DatabaseManager;
 import exception.ResponseException;
 import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
 import server.ServerFacade;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,9 +37,27 @@ public class ServerFacadeTests {
 
 
     @Test
+    public void testClearDatabaseSuccess() throws Exception {
+        var authData = facade.registerUser(new UserData("username", "password", "email"));
+
+        facade.clearDatabase();
+
+        try (Connection connection = DatabaseManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM authTokens")) {
+            var result = statement.executeQuery();
+            assertFalse(result.next());
+        }
+        try (Connection connection = DatabaseManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM users")) {
+            var result = statement.executeQuery();
+            assertFalse(result.next());
+        }
+    }
+
+    @Test
     public void testRegisterUserSuccess() throws Exception {
-        var userData = facade.registerUser(new UserData("username", "password", "email"));
-        assertNotNull(userData.authToken());
-        assertEquals("username", userData.username());
+        var authData = facade.registerUser(new UserData("username", "password", "email"));
+        assertNotNull(authData.authToken());
+        assertEquals("username", authData.username());
     }
 }
