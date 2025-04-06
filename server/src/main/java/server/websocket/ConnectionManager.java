e0521551ebcaf3342fb6558eb99c578b2da4a5b4
cashcraft2 <cashcraft2@gmail.com>
2025-04-05 21:27:37 -0600

    Updated ConnectionManager that ensures messages stay within games

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -2,26 +2,37 @@ package server.websocket;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+    public final ConcurrentHashMap<Integer, Set<Connection>> connections = new ConcurrentHashMap<>();
 
-    public void add(String username, Session session) {
+    public void add(int gameID, String username, Session session) {
         var connection = new Connection(username, session);
-        connections.put(username, connection);
+        connections.computeIfAbsent(gameID, key -> ConcurrentHashMap.newKeySet()).add(connection);
     }
 
-    public void remove(String username) {
-        connections.remove(username);
+    public void remove(int gameID, String username) {
+        var newConnections = connections.get(gameID);
+        if (newConnections != null) {
+            newConnections.removeIf(connection-> connection.username.equals(username));
+            if (newConnections.isEmpty()) {
+                connections.remove(gameID);
+            }
+        }
     }
 
-    public void broadcast(String excludedUser, ServerMessage message) throws IOException {
+    public void broadcast(int gameID, String excludedUser, ServerMessage message) throws IOException {
+        var newConnections = connections.get(gameID);
+        if (newConnections == null){
+            return;
+        }
         var removeList = new ArrayList<Connection>();
 
-        for (var connection : connections.values()) {
+        for (var connection : newConnections) {
             if (connection.session.isOpen()) {
                 if(!connection.username.equals(excludedUser)) {
                     connection.send(message.toString());
@@ -31,8 +42,9 @@ public class ConnectionManager {
                 removeList.add(connection);
             }
         }
-        for (var connection : removeList) {
-            connections.remove(connection.username);
+        newConnections.removeAll(removeList);
+        if (newConnections.isEmpty()) {
+            connections.remove(gameID);
         }
     }
 }
